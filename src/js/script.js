014e875c94893d271204f88db109016db6f0d4dd
alexander <alxndrklim@gmail.com>
2021-02-10 23:41:49 +0300

    add new adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
         responsive: [{
-            breakpoint: 992,
+            breakpoint: 1200,
             settings: {
                 dots: true,
                 arrows: false
