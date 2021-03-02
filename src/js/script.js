337edf51ee53948b7420984924a59c492f21c9a3
alexander <alxndrklim@gmail.com>
2021-03-02 11:12:33 +0300

    delete php file

--- src/js/script.js
+++ src/js/script.js
@@ -80,7 +80,7 @@ $(document).ready(function() {
             rules: {
                 name: {
                     required: true,
-                    minlingth: 2
+                    minlength: 2
                 },
                 phone: "required",
                 email: {
@@ -107,21 +107,4 @@ $(document).ready(function() {
     validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
-
-    $('form').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-            $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn('slow');
-
-            $('form').trigger('reset');
-        });
-        return false;
-    });
-
 });
